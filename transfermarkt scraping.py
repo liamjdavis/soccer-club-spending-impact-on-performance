9e33f6faee9f8e7b2a889179c614fb0db3a18de0
liamjdavis <66973153+liamjdavis@users.noreply.github.com>
2023-08-11 11:44:35 -0700

    Add files via upload

--- transfermarkt scraping.py
+++ transfermarkt scraping.py
@@ -1,27 +1,42 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-url = 'https://www.transfermarkt.com/transfers/einnahmenausgaben/statistik/plus/1?ids=a&sa=&saison_id=2017&saison_id_bis=2022&land_id=&nat=&kontinent_id=&pos=&altersklasse=&w_s=&leihe=&intern=0&plus=1'
+# Initialize the WebDriver (replace 'chrome' with 'firefox' if using Firefox)
+driver = webdriver.Chrome()
 
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
-expenditureTable = soup.find(class_ = 'responsive-table')
+# Open the URL
+url = "https://www.transfermarkt.com/transfers/einnahmenausgaben/statistik/a/ids/a/sa//saison_id/2017/saison_id_bis/2022/land_id/0/nat/0/kontinent_id/0/pos//w_s//intern/0"
+driver.get(url)
 
-if expenditureTable:
-    clubs = []
-    expenditures = []
+# Wait for the table to load
+wait = WebDriverWait(driver, 20)
+table = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".responsive-table")))
 
-    for row in expenditureTable.find_all('tr')[1:]:
-        columns = row.find_all('td')
-        clubName = columns[2].text.strip()
-        clubExpenditure = columns[4].text.strip()
-        clubs.append(clubName)
-        expenditures.append(clubExpenditure)
+# Find all rows in the table
+rows = table.find_elements(By.CSS_SELECTOR, "tbody tr")
 
-    data = {'Club': clubs, 'Expenditure': expenditures}
-    df = pd.DataFrame(data)
+# Create a list to store club expenditures data
+club_expenditures_data = []
 
-    df.to_csv('club expenditures.csv', index=False)
-else: 
-    print("expenditure table not found")
+# Iterate through each row and extract data
+for row in rows:
+    columns = row.find_elements(By.TAG_NAME, "td")
+    if len(columns) >= 3:
+        club_name = columns[2].text
+        expenditure = columns[3].text
+        club_expenditures_data.append([club_name, expenditure])
+
+# Write the data to the CSV file (append to existing data)
+csv_file = "club expenditures.csv"
+with open(csv_file, mode="a", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    for row in club_expenditures_data:
+        writer.writerow(row)  # Write each row separately
+
+# Close the WebDriver
+driver.quit()
+
+print(f"Data appended to {csv_file}")
